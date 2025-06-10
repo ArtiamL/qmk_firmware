ca6aa3f3c88914fa57fc37fa99aef045ab5e74e9
Luke Newell <main.lnewell@gmail.com>
2025-06-10 15:58:51 +0100

    Adjusted keymap

--- keyboards/golden_rascal/config.h
+++ keyboards/golden_rascal/config.h
@@ -1,8 +1,8 @@
 #define EE_HANDS
 
-#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
+// #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
 #define SERIAL_USART_TX_PIN GP0     // USART TX pin
-#define SERIAL_USART_RX_PIN GP1     // USART RX pin
+// #define SERIAL_USART_RX_PIN GP1     // USART RX pin
 
 #define MATRIX_ROW_PINS { GP8, GP9, GP10, GP19 }
 #define MATRIX_COL_PINS { GP3, GP4, GP5, GP6, GP7 }
@@ -14,6 +14,6 @@
 
 #define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
-#define TAPPING_TOGGLE 2
+// #define TAPPING_TOGGLE 2
 
 #define CAPS_WORD_INVERT_ON_SHIFT
--- keyboards/golden_rascal/keymaps/default/keymap.c
+++ keyboards/golden_rascal/keymaps/default/keymap.c
@@ -34,9 +34,10 @@ enum combos {
 
 enum layer_names {
   _BASE,
+  _PROG,
+  _FN,
   _GAMING,
   _GAMING_NUMS,
-  _PROG
 };
 
 void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
@@ -113,30 +114,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
     [_BASE] = LAYOUT_split_3x5_3(
         KC_Q,               KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,               KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        LGUI_T(KC_A),       KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         LT(_PROG, KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                        KC_LSFT, ALT_T(KC_SPC), KC_LCTL,           KC_BSPC,  KC_ENT,  TT(_PROG)
+                                        KC_LSFT, KC_SPC, KC_LCTL,           KC_BSPC,  KC_ENT,  TT(_PROG)
     ),
 
     [_PROG] = LAYOUT_split_3x5_3(
-        KC_TRNS,    KC_7,    KC_8,    KC_9,    KC_0,                                  TD(MINS_UNDS),    TD(EQL_PLS),    KC_UP,      TD(LBRC_LCBR),    TD(RBRC_RCBR),
-        KC_TRNS,    KC_4,    KC_5,    KC_6,    KC_DEL,                                TD(BSLS_PIPE),    KC_LEFT,        KC_DOWN,    KC_RIGHT,         TD(NUHS_TILD),
-        KC_TRNS,    KC_1,    KC_2,    KC_3,    CW_TOGG,                               TD(QUOT_AT),      KC_HOME,        KC_END,     UK_GRV,           KC_TRNS,
-                                      KC_TRNS, KC_TRNS, KC_TRNS,           TD(QUOT_AT),  KC_MPLY,  KC_TRNS
+        KC_TAB,     KC_7,    KC_8,    KC_9,    KC_0,                                  TD(MINS_UNDS),    TD(EQL_PLS),    KC_UP,      TD(LBRC_LCBR),    TD(RBRC_RCBR),
+        KC_LGUI,    KC_4,    KC_5,    KC_6,    KC_DEL,                                TD(BSLS_PIPE),    KC_LEFT,        KC_DOWN,    KC_RIGHT,         TD(NUHS_TILD),
+        KC_TRNS,    KC_1,    KC_2,    KC_3,    TG(_GAMING),                           TD(QUOT_AT),      KC_HOME,        KC_END,     UK_GRV,           MO(_FN),
+                                      KC_TRNS, KC_LALT, KC_TRNS,            KC_TRNS,  KC_MPLY,  KC_TRNS
+    ),
+
+    [_FN] = LAYOUT_split_3x5_3(
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                                KC_F1,    KC_F2,    KC_F3,     KC_F4,     KC_F5,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                                KC_F6,    KC_F7,    KC_F8,     KC_F9,     KC_F10,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                                KC_F11,   KC_F12,   KC_MPRV,   KC_MNXT,   KC_TRNS,
+                                                  KC_TRNS, KC_TRNS, KC_TRNS,            TG(_GAMING),  KC_TRNS,  KC_TRNS
     ),
 
     [_GAMING] = LAYOUT_split_3x5_3(
-        KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,                               KC_T,    KC_Y,    KC_U,    KC_I,       KC_O,
-        KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,                               KC_G,    KC_H,    KC_J,    KC_K,       KC_L,
-        KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,                               KC_B,    KC_N,    KC_M,    KC_COMM,    KC_DOT,
-                            MO(_GAMING_NUMS),  KC_SPC,  KC_SPC,           KC_P,  KC_SCLN,  KC_SLSH
+        KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+                          MO(_GAMING_NUMS),  KC_SPC,  KC_LALT,           KC_T,  KC_G,  LT(_PROG, KC_B)
     ),
 
     [_GAMING_NUMS] = LAYOUT_split_3x5_3(
-        KC_1,        KC_2,       KC_TRNS,    KC_3,       KC_4,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_5,        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_6,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-        KC_TRNS,     KC_7,       KC_8,       KC_9,       KC_0,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
-                                              KC_TRNS,  KC_TRNS,  KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
+        KC_1,            KC_2,       KC_TRNS,    KC_3,       KC_4,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+        KC_5,            KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_6,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+        TG(_GAMING),     KC_7,       KC_8,       KC_9,       KC_0,                               KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+                                            KC_TRNS,  KC_TRNS,  KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
 
 };
@@ -150,10 +158,10 @@ const uint16_t PROGMEM sg_combo[] = {KC_S, KC_G, COMBO_END};
 
 combo_t key_combos[] = {
   [AB_ESC] = COMBO(ab_combo, KC_ESC),
-  [AS_TAB] = COMBO(as_combo, KC_TAB),
+  //[AS_TAB] = COMBO(as_combo, KC_TAB),
   //[QW_ALTTAB] = COMBO(qw_combo, ALT_TAB),
-  [CX_LGUI] = COMBO(cx_combo, KC_LGUI),
-  [VB_LAYER] = COMBO(vb_combo, TG(_GAMING)),
+  // [CX_LGUI] = COMBO(cx_combo, KC_LGUI),
+  // [VB_LAYER] = COMBO(vb_combo, TG(_GAMING)),
   //[SG_LAYER] = COMBO(sg_combo, MO(1)),
 };
 
